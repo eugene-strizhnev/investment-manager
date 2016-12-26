6734d4109548c03e1371f4f916819cb29ad80509
Yevhen Strizhnev <Yevhen.Strizhnev@thomsonreuters.com>
2016-12-26 19:04:35 +0300

    first commit

---
+++ public/javascripts/data/data.js
@@ -0,0 +1,24 @@
+export default {
+    news: [
+        {
+            title: "title 1",
+            body: "body 1",
+            author: "author 1",
+        },
+        {
+            title: "title 2",
+            body: "body 2",
+            author: "author 2",
+        },
+        {
+            title: "title 3",
+            body: "body 3",
+            author: "author 3",
+        },
+        {
+            title: "title 4",
+            body: "body 4",
+            author: "author 4",
+        }
+    ]
+}
