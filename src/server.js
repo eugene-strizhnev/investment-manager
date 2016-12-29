aa46bbbd83f48f037bffe4ad253ecc11aa06fc93
Yevhen Strizhnev <Yevhen.Strizhnev@thomsonreuters.com>
2016-12-29 18:23:17 +0300

    added several more pages.

--- src/server.js
+++ src/server.js
@@ -1,47 +1,51 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+import express  from 'express';
+import React    from 'react';
+import ReactDom from 'react-dom/server';
+import {match, RouterContext} from 'react-router';
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+import routes from './routes';
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.use((req, res) => {
+    match({routes, location: req.url}, (error, redirectLocation, renderProps) => {
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+        if (redirectLocation) { // Если необходимо сделать redirect
+            return res.redirect(301, redirectLocation.pathname + redirectLocation.search);
+        }
 
-app.use('/', index);
-app.use('/users', users);
+        if (error) { // Произошла ошибка любого рода
+            return res.status(500).send(error.message);
+        }
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+        if (!renderProps) { // Мы не определили путь, который бы подошел для URL
+            return res.status(404).send('Not found');
+        }
+
+        const componentHTML = ReactDom.renderToString(<RouterContext {...renderProps} />);
+        return res.end(renderHTML(componentHTML));
+    });
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+const assetUrl = process.env.NODE_ENV !== 'production' ? 'http://localhost:8050' : '/';
 
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
-});
+function renderHTML(componentHTML) {
+    return `
+    <!DOCTYPE html>
+      <html>
+      <head>
+          <meta charset="utf-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>Hello React</title>
+          <link rel="stylesheet" href="${assetUrl}/public/assets/styles.css">
+      </head>
+      <body>
+        <div id="react-view">${componentHTML}</div>
+        <script type="application/javascript" src="${assetUrl}/public/assets/bundle.js"></script>
+      </body>
+    </html>
+  `;
+}
 
 app.listen(process.env.PORT || 3000);
-module.exports = app;
 
